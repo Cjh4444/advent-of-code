98ca6dd45ee499e4172945826ec7b05468a899d8
Camden Harris <camdenharris@gmail.com>
2024-12-24 21:40:28 -0800

    day 14 method that isn't really efficient but would have worked, just board animation in terminal, seeing dots though

--- Day14/star2.py
+++ Day14/star2.py
@@ -1,4 +1,5 @@
 import os
+import time
 board_x = 101
 board_y = 103
 
@@ -28,18 +29,26 @@ with open("data.txt") as f:
         
         x, y, vx, vy = int(x), int(y), int(vx), int(vy)
         
+        x,y = robot_movement_after_seconds_eff(x,y,vx,vy,8000)
+        
         robots.append([x,y,vx,vy])
     
-    loop = 0
+    loop = 8000
     while True:
         board = [["."] * board_x for _ in range(board_y)]
         for robot in robots:
             x,y,vx,vy = robot
-            board[y][x] = "X"
-            robot[1], robot[0] = move_robot(x,y,vx,vy)
+            robot[0], robot[1] = move_robot(x,y,vx,vy)
+            
+            try:
+                board[y][x] = "X"
+            except:
+                raise Exception(y,x, "out of range")
+            
         
         for line in board:
             print("".join(line))
         print(f"{loop=}")
+        time.sleep(.1)
         loop += 1
         os.system("clear")
