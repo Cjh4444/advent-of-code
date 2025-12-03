285d1e8cce79a91b188dc57abc6f08d7d3c508c6
Cjh4444 <camdenharris@gmail.com>
2025-12-03 01:52:58 -0800

    changed day 3 part 2 to make function general for any n length joltage

--- Day03/star2.py
+++ Day03/star2.py
@@ -5,9 +5,9 @@ def pack_nums(voltages: list[int]):
     return ret
 
 
-def largest_joltage(batteries: list[int]):
-    largest_joltage = batteries[:12]
-    suffix = batteries[12:]
+def largest_joltage(batteries: list[int], length):
+    largest_joltage = batteries[:length]
+    suffix = batteries[length:]
     for i in suffix:
         for j in range(len(largest_joltage) - 1):
             if largest_joltage[j] < largest_joltage[j + 1]:
@@ -15,20 +15,24 @@ def largest_joltage(batteries: list[int]):
                 break
 
         largest_joltage.append(i)
-        if len(largest_joltage) > 12:
+        if len(largest_joltage) > length:
             largest_joltage.remove(min(largest_joltage))
 
     return pack_nums(largest_joltage)
 
 
-def process(file):
+def process(file, length):
     with open(file) as f:
-        return sum(largest_joltage([int(num) for num in bank.strip()]) for bank in f)
+        return sum(
+            largest_joltage([int(num) for num in bank.strip()], length=length)
+            for bank in f
+        )
 
 
-assert largest_joltage([int(num) for num in "987654321111111"]) == 987654321111
-assert largest_joltage([int(num) for num in "811111111111119"]) == 811111111119
-assert largest_joltage([int(num) for num in "234234234234278"]) == 434234234278
-assert largest_joltage([int(num) for num in "818181911112111"]) == 888911112111
-assert process("test.txt") == 3121910778619
-print(process("data.txt"))
+assert largest_joltage([int(num) for num in "987654321111111"], 12) == 987654321111
+assert largest_joltage([int(num) for num in "811111111111119"], 12) == 811111111119
+assert largest_joltage([int(num) for num in "234234234234278"], 12) == 434234234278
+assert largest_joltage([int(num) for num in "818181911112111"], 12) == 888911112111
+assert process("test.txt", 2) == 357
+assert process("test.txt", 12) == 3121910778619
+print(process("data.txt", 12))
